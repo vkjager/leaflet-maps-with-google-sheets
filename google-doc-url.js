776a062fe1edcdd52ed51d3e47a5b14ddc296e64
vkjager <46757396+vkjager@users.noreply.github.com>
2019-01-16 11:56:58 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GJQFG3CIbRcJj0yyfXshJS2n5o4F56uymQTUiP27OIo/edit#gid=0';
